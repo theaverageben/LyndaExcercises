7dcc108bba137379eb18b86918484564bff10154
Ben Allen <benallen1306@gmail.com>
2020-10-18 22:28:35 +0100

    Create dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,13 @@
+# California state symbols
+# stateBird = 'california quail'
+# stateAnimal = 'Grizzly bear'
+# stateFlower = 'California poppy'
+# stateFruit = 'Avocado'
+
+california_symbols = {
+    'bird': ' california qual',
+    'animal':'Grizzly bear',
+    'flower':'California poppy',
+    'fruit':'Avocado',
+}
+print(california_symbols['flower'])
