502743903a80ac8d606082ef0bd8733e62e02dbb
Ben Allen <benallen1306@gmail.com>
2020-10-18 22:54:50 +0100

    Create collections.py

---
+++ collections.py
@@ -0,0 +1,33 @@
+# star1 = 'Sol'
+# star2 = 'Alpha Centauri'
+# star3 = 'Barnard'
+# star4 = 'Wolf 359'
+
+# Highest peak on each tectonic plate
+# African = 'Kilimanjaro'
+# Antarctic = 'Vinson'
+# Australian = 'Puncak Jaya'
+# Eurasian = 'Everest'
+# North_American = 'Denali'
+# Pacific = 'Mauna Kea'
+# South_American = 'Aconcagua'
+
+stars = [
+    'Sol',
+    'Alpha Centauri',
+    'Barnard',
+    'Wolf 359',
+]
+print(stars[3])
+
+peaks = {
+    'African' : 'Kilimanjaro',
+    'Antarctic' : 'Vinson',
+    'Australian' : 'Puncak Jaya',
+    'Eurasian' : 'Everest',
+    'North_American' : 'Denali',
+    'Pacific' : 'Mauna Kea',
+    'South_American' : 'Aconcagua',
+}
+
+print(peaks['African'])
