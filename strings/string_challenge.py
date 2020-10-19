824240b0280faa341fe87157723de17d7c2bdfb9
Ben Allen <benallen1306@gmail.com>
2020-10-19 19:54:36 +0100

    Create string_challenge.py

---
+++ strings/string_challenge.py
@@ -0,0 +1,5 @@
+miles = input('Enter a distance in miles: ')
+# kilometers_value = miles_value * 1.609344
+miles_int=int(miles)
+print(miles + ' miles converted into Kilometers is: ')
+print(miles_int * 1.609344)
